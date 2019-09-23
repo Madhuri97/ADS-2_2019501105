3cf234f692ef7c224a1aa21c0fecdbf67069d584
madhuri <madhiurinaidu1997@msitprogram.net>
2019-09-23 20:24:08 +0530

    by madhuri

---
+++ CSPP-2_2019501105/Day-1/Assignment-4/AbsoluteNumber.java
@@ -0,0 +1,23 @@
+/**
+ * Given n, find the absolute value of n.
+ * 
+ * Input : abs(-1)
+ * Output : 1
+ * 
+ * Input : abs(0)
+ * Output : 0
+ * 
+ * @author Siva Sankar
+ */
+
+public class AbsoluteNumber {
+
+    /**
+     * This method returns the absolute number of the given n
+     * @param n, the integer
+     * @return absolute value of n
+     */
+    public static int abs(int n) {
+        // Your code goes here....
+    }
+}
